278ef1e8e5187acda6e1aa36ed256ed27dae692b
Thomas Erlang <thomas@erlang.dk>
2023-03-23 19:59:06 +0100

    Removed offset from subtitles doesn't seem to work as expected

--- seplis_play_server/transcoders/subtitle.py
+++ seplis_play_server/transcoders/subtitle.py
@@ -3,16 +3,12 @@ from typing import Dict
 from seplis_play_server import config, logger
 from .video import stream_index_by_lang, subprocess_env, to_subprocess_arguments
 
-async def get_subtitle_file(metadata: Dict, lang: str, start_time: int, offset: int = None):
+async def get_subtitle_file(metadata: Dict, lang: str, start_time: int):
     if not lang:
         return
     sub_index = stream_index_by_lang(metadata, 'subtitle', lang)
     if not sub_index:
         return
-    if offset and start_time and (offset < 0):
-        if start_time > offset:
-            start_time += offset
-            offset = 0
     args = [
         {'-analyzeduration': '20000000'},
         {'-probesize': '20000000'},
@@ -26,8 +22,6 @@ async def get_subtitle_file(metadata: Dict, lang: str, start_time: int, offset: 
         {'-f': 'webvtt'},
         {'-': None},
     ]
-    if offset:
-        args.insert(2, {'-itsoffset': str(offset)})
     args = to_subprocess_arguments(args)        
     logger.debug(f'Subtitle args: {" ".join(args)}')
     process = await asyncio.create_subprocess_exec(
