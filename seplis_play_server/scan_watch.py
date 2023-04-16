b04b8d0effb64ba9321c25b9f775e8c5653b97f9
Thomas Erlang <thomas@erlang.dk>
2023-04-16 19:38:30 +0200

    Added queue for scan watch file changes and upgraded watchfiles

--- seplis_play_server/scan_watch.py
+++ seplis_play_server/scan_watch.py
@@ -3,58 +3,80 @@ import os
 from watchfiles import awatch, Change
 from seplis_play_server.scanners import Movie_scan, Episode_scan, Play_scan
 from seplis_play_server import config, logger
+from seplis_play_server.config import ConfigPlayScanModel
+
+files_waiting_to_finish = {}
+
+scan_queue = asyncio.Queue()
 
 
 async def main():
-    scanners: dict[str, scan.Play_scan] = {}
+    w = asyncio.create_task(worker(scan_queue))
     for scan in config.scan:
-        if scan.type == 'series':
-            scanners[scan.path] = Episode_scan(
-                scan_path=scan.path, 
-                make_thumbnails=scan.make_thumbnails,
-                parser=scan.parser,
-            )
-        elif scan.type == 'movies':
-            scanners[scan.path] = Movie_scan(
-                scan_path=scan.path, 
-                make_thumbnails=scan.make_thumbnails,
-                parser=scan.parser, 
-            )
-    waiting = {}
+        logger.info(f'Watching: {scan.path} ({scan.type})')
     async for changes in awatch(*[str(scan.path) for scan in config.scan]):
-        scanner: scan.Play_scan = None
         for c in changes:
-            changed, path = c
-            for base_path in scanners:
-                if path.lower().startswith(str(base_path).lower()):
-                    scanner = scanners[base_path]
+            changed, path = c       
+            scan_info = None     
+            for scan in config.scan:
+                if path.lower().startswith(str(scan.path).lower()):
+                    scan_info = scan
                     break
-            if path in waiting:
-                waiting[path].cancel()
-                waiting.pop(path)
+            logger.debug(f'{changed.name}: {path} ({scan_info.type})')
+            # if the file is being writtin it will trigger a change event multiple times
+            # so we try and wait for the file to be written before parsing it.
+            if path in files_waiting_to_finish:
+                files_waiting_to_finish[path].cancel()
+                files_waiting_to_finish.pop(path)
                 
-            waiting[path] = asyncio.create_task(parse( 
-                scanner=scanner, 
+            files_waiting_to_finish[path] = asyncio.create_task(parse(
                 path=path, 
-                changed=changed, 
-                waiting=waiting
+                changed=changed,
+                scan_info=scan_info,
             ))
 
 
-async def parse(scanner: Play_scan, path: str, changed: Change, waiting: dict):
+def get_scanner(scan: ConfigPlayScanModel) -> Play_scan:
+    if scan.type == 'series':
+        return Episode_scan(
+            scan_path=scan.path, 
+            make_thumbnails=scan.make_thumbnails,
+            parser=scan.parser,
+        )
+    elif scan.type == 'movies':
+        return Movie_scan(
+            scan_path=scan.path, 
+            make_thumbnails=scan.make_thumbnails,
+            parser=scan.parser, 
+        )
+
+
+async def worker(queue: asyncio.Queue):
+    while True:
+        item = await queue.get()
+        if item is None:
+            break
+        changed, path, scan_info = item
+        scanner = get_scanner(scan_info)
+        logger.info(f'{changed.name}: {path} ({scan_info.type})')
+        parsed = scanner.parse(path)
+        if parsed:
+            if changed in (Change.added, Change.modified):
+                await scanner.save_item(parsed, path)
+            elif changed == Change.deleted:
+                await scanner.delete_path(path)
+        else:
+            logger.warning(f'Unknown: {path}')
+        queue.task_done()
+
+
+async def parse(path: str, changed: Change, scan_info: ConfigPlayScanModel):
+    # wait for the file to be fully written
     if changed in (Change.added, Change.modified):
         await asyncio.sleep(3)
-    waiting.pop(path, None)
+    files_waiting_to_finish.pop(path, None)
     info = os.path.splitext(path)
     if len(info) != 2:
         return
-    if info[1][1:].lower() not in config.media_types:
-        return
-    parsed = scanner.parse(path)
-    if parsed:
-        if changed in (Change.added, Change.modified):
-            logger.info(f'Added/changed: {path}')
-            await scanner.save_item(parsed, path)
-        elif changed == Change.deleted:
-            logger.info(f'Deleted: {path}')
-            await scanner.delete_path(path)
+    if info[1][1:].lower() in config.media_types:
+        await scan_queue.put((changed, path, scan_info))
